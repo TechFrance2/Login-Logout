05a51f597cbec45292326420cc9780e651735129
Franci Nakamura <franci.nakamura100@gmail.com>
2020-06-27 09:07:11 -0700

    initialized project & added button and text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super ()
+      this.state = {
+        isLoggedIn: false
+      }
+      this.handleClick = this.handleClick.bind (this)
+    }
+    handleClick () {
+      this.setState (prevState => {
+        return {
+          isLoggedIn: !prevState.isLoggedIn
+        }
+      })
+    }
+  render () {
+    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+    let displayText = this.state.isLoggedIn ? "YOU ARE LOGGED IN" : "YOU ARE LOGGED OUT"
+    return (
+      <div>
+        <button onClick = {this.handleClick}>{buttonText}</button>
+        <h1> {displayText}</h1>
+      </div>
+    )
+  }
 }
-
 export default App;
